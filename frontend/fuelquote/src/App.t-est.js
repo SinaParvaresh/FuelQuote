072afcc3bf8ef90a3f88b046b7c0628b6865d515
Ankur Sharma <33098281+Ankur2698@users.noreply.github.com>
2022-05-06 16:52:16 -0500

    Removed unnecessary files

--- frontend/fuelquote/src/App.t-est.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Checks that homepage loads correctly when app starts', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Welcome to the Fuel Quote Website of GROUP 33!!!/);
-  expect(linkElement).toBeInTheDocument();
-});
